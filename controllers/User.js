d3c807d482cfa0b245e18a47572daced787dc512
kivo360 <kivo360@gmail.com>
2015-02-07 18:26:55 -0500

    Fixed bad logic in login

--- controllers/User.js
+++ controllers/User.js
@@ -74,7 +74,7 @@ exports.postRegister = function (req, res) {
                 function (user, callback) {
                     //console.log(user);
                     if(!user){
-                        callback(null, {message: "The User With The Number " + req.param('phone') + " Already Exist"}, true)
+                        return {message: "The User With The Number " + req.param('phone') + " Already Exist"};
                     }
                     gremtool.create(user, function (err, use) {
                         callback(null, use, false);
